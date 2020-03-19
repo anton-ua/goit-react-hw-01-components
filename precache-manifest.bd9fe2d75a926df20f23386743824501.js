6a3c71e0a3480aa3752214394b63509f4cb31ff5
anton-ua <antonyua23@gmail.com>
2020-03-19 13:27:17 +0200

    Updates

---
+++ precache-manifest.bd9fe2d75a926df20f23386743824501.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cab3d93e402d1d3f3bece8d936f0431d",
+    "url": "/anton-ua/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "9b818783d46f69c7baa2",
+    "url": "/anton-ua/goit-react-hw-01-components/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dffb27a348903c2fce0a",
+    "url": "/anton-ua/goit-react-hw-01-components/static/js/2.a44324a5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/anton-ua/goit-react-hw-01-components/static/js/2.a44324a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b818783d46f69c7baa2",
+    "url": "/anton-ua/goit-react-hw-01-components/static/js/main.f24931f8.chunk.js"
+  },
+  {
+    "revision": "e4050464046d911f9130",
+    "url": "/anton-ua/goit-react-hw-01-components/static/js/runtime-main.d85d92d3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/anton-ua/goit-react-hw-01-components/static/media/logo.5d5d9eef.svg"
+  }
+]);
